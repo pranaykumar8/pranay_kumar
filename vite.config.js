5dbc8edfbb1e16d7e1b4e213fdc3135cdf5009de
pranaykumar8 <pranayraos9@gmail.com>
2025-08-30 13:29:14 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -44,6 +44,6 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my_portfolio/', // ✅ Correct path for GitHub Pages
+  base: '/pranay_kumar/', // ✅ Correct path for GitHub Pages
 });
 
